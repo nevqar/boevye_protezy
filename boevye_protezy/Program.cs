b4353459464bd67c81afdbfc1d2f302153e846e2
nev-ar <nev_ar@mail.ru>
2025-12-05 22:43:14 +0300

    Добавьте файлы проекта.

---
+++ boevye_protezy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace boevye_protezy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
