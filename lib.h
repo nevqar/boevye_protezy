eb2ab008e41c1c65b35679a0551d953e5769fac7
Dim404-s <savdimam@gmail.com>
2025-12-06 10:52:48 +0300

    Add files via upload
    test mouse_control commit

---
+++ lib.h
@@ -0,0 +1,28 @@
+class Mouse_controller
+{
+    public:
+     void SetCursorPosition(int x, int y)
+    {
+
+    }
+     void LeftСlick()
+    {
+
+    }
+    //Нажимает ПКМ
+    void RightClick()
+    {
+
+    }
+    //Возвращает ширину окна в пикселях
+    int GetWindowWidth()
+    {
+      return -1;
+    }
+    int GetWindowHeight()
+    {
+      return -1;
+    }
+
+
+};
---
+++ mouse.cpp
@@ -0,0 +1,7 @@
+#include "iostream"
+#include "lib.h"
+int main(){
+    Mouse_controller mouse;
+    
+    return 0;
+}
