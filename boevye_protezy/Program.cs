dd733459ed950fe12236311de135439a1c57e805
nev-ar <nev_ar@mail.ru>
2025-12-06 09:26:05 +0300

    исправление: добавлено удаление сканера после использоваания

--- boevye_protezy/Program.cs
+++ boevye_protezy/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using CppClasses;
+﻿using CppClasses;
+using NeuroSDK;
+using System;
 namespace boevye_protezy
 {
 	internal class Program
@@ -9,6 +10,17 @@ namespace boevye_protezy
 			Console.WriteLine("Hello, World!");
 			MouseController mc = new MouseController();
 			Console.WriteLine(mc.GetHelloWorld());
+
+			IReadOnlyList<SensorInfo> sensors = Sensor.Scan(1500);
+			if (sensors.Count == 0)
+				throw new Exception("No sensors found!");
+			for (int i = 0; i < sensors.Count; i++)
+			{
+				Console.WriteLine(i + "\t" + sensors[i].Name + "\t" + sensors[i].Address);
+			}
+			Console.Write("Выберите сенсор >> ");
+			SensorInfo sensor = sensors[int.Parse(Console.ReadLine())];
+
 		}
 	}
 }
---
+++ boevye_protezy/Sensor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NeuroSDK;
+
+namespace boevye_protezy
+{
+	public static class Sensor
+	{
+		public static IReadOnlyList<SensorInfo> Scan(int delay)
+		{
+			Scanner scanner = new Scanner(SensorFamily.SensorLECallibri);
+			scanner.Start();
+			System.Threading.Thread.Sleep(delay);
+			scanner.Stop();
+			return scanner.Sensors;
+			scanner.Dispose();
+		}
+	}
+}
