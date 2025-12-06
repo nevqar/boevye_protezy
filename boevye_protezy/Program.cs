7347564b61078788613484282f202571f2878ab2
nev-ar <nev_ar@mail.ru>
2025-12-06 11:57:51 +0300

    тык

--- boevye_protezy/Program.cs
+++ boevye_protezy/Program.cs
@@ -7,17 +7,54 @@ namespace boevye_protezy
 	{
 		static void Main()
 		{
-			CallibriSensor sensor = Sensor.GetSensor(3000);
+			CallibriSensor sensor = Sensor.GetSensor(4000);
 			Console.WriteLine(sensor);
 
-			var commands = sensor.Commands;
-			for (int i = 0; i < commands.Count; i++)
-			{
-				Console.WriteLine(commands[i]);
-			}
+			//sensor.EventMEMSDataRecived += Sensor_EventMEMSDataRecived;
+			//sensor.ExecCommand(SensorCommand.CommandStartMEMS);
+
+			//Console.Clear();
+
+			//System.Threading.Thread.Sleep(1000000);
+
+			//sensor.EventMEMSDataRecived -= Sensor_EventMEMSDataRecived;
+			//sensor.ExecCommand(SensorCommand.CommandStopMEMS);
+
+
+			sensor.EventQuaternionDataRecived += Sensor_EventQuaternionDataRecived;
+			sensor.ExecCommand(SensorCommand.CommandStartAngle);
+			Console.Clear();
+			System.Threading.Thread.Sleep(1000000);
+			sensor.EventQuaternionDataRecived -= Sensor_EventQuaternionDataRecived;
+			sensor.ExecCommand(SensorCommand.CommandStopAngle);
+
+
 
 			sensor.Disconnect();
 			sensor.Dispose();
+		}
+		static private void Sensor_EventMEMSDataRecived(ISensor sensor, MEMSData[] data)
+		{
+			for (int i = 0; i < data.Length; i++)
+			{
+				Console.SetCursorPosition(0, 0);
+				Console.WriteLine(data[i].Accelerometer.X);
+				Console.WriteLine(data[i].Accelerometer.Y);
+				Console.WriteLine(data[i].Accelerometer.Z);
+				Console.WriteLine();
+			}
+		}
+		private static void Sensor_EventQuaternionDataRecived(ISensor sensor, QuaternionData[] data)
+		{
+			for (int i = 0; i < data.Length; i++)
+			{
+				Console.SetCursorPosition(0, 0);
+				Console.WriteLine(data[i].W);
+				Console.WriteLine(data[i].X);
+				Console.WriteLine(data[i].Y);
+				Console.WriteLine();
+
+			}
 		}
 	}
 }
