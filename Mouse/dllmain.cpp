a9ffe9e8fe922b55bc1e7f69773378611586c7d5
VMK65490 <kartavenkovladimir0@gmail.com>
2025-12-06 11:47:40 +0300

    вдд

--- Mouse/dllmain.cpp
+++ Mouse/dllmain.cpp
@@ -1,6 +1,7 @@
 ﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
 // для Вовы
 #include "pch.h"
+#include "dllmain.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -18,3 +19,23 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+
+// mydll.cpp
+#include "dllmain.h"
+#include <iostream>
+#include <string>
+
+// Функция сложения
+MYDLL_API int add(int a, int b) {
+    return a + b;
+}
+
+// Функция умножения
+MYDLL_API int multiply(int a, int b) {
+    return a * b;
+}
+
+// Функция приветствия
+MYDLL_API void greet(const char* name) {
+    std::cout << "Привет, " << name << "!" << std::endl;
+}
---
+++ Mouse/dllmain.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef MYDLL_H
+#define MYDLL_H
+
+// Если компилируем DLL, экспортируем функции
+#ifdef MYDLL_EXPORTS
+#define MYDLL_API __declspec(dllexport)
+#else
+#define MYDLL_API __declspec(/*dllimport*/)
+#endif
+
+// Простые функции для примера
+extern "C" MYDLL_API int add(int a, int b);
+extern "C" MYDLL_API int multiply(int a, int b);
+extern "C" MYDLL_API void greet(const char* name);
+
+#endif
