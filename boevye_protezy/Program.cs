f50df6aef0397cd647f6fae8e5e5dfa099f93795
nev-ar <nev_ar@mail.ru>
2025-12-06 10:19:03 +0300

    переделан метод получения сенсора

--- boevye_protezy/Program.cs
+++ boevye_protezy/Program.cs
@@ -5,22 +5,10 @@ namespace boevye_protezy
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Console.WriteLine("Hello, World!");
-			MouseController mc = new MouseController();
-			Console.WriteLine(mc.GetHelloWorld());
-
-			IReadOnlyList<SensorInfo> sensors = Sensor.Scan(1500);
-			if (sensors.Count == 0)
-				throw new Exception("No sensors found!");
-			for (int i = 0; i < sensors.Count; i++)
-			{
-				Console.WriteLine(i + "\t" + sensors[i].Name + "\t" + sensors[i].Address);
-			}
-			Console.Write("Выберите сенсор >> ");
-			SensorInfo sensor = sensors[int.Parse(Console.ReadLine())];
-
+			CallibriSensor s = Sensor.GetSensor(3000);
+			Console.WriteLine(s);
 		}
 	}
 }
--- boevye_protezy/Sensor.cs
+++ boevye_protezy/Sensor.cs
@@ -7,14 +7,32 @@ namespace boevye_protezy
 {
 	public static class Sensor
 	{
-		public static IReadOnlyList<SensorInfo> Scan(int delay)
+		public static CallibriSensor GetSensor(int delay)
 		{
 			Scanner scanner = new Scanner(SensorFamily.SensorLECallibri);
 			scanner.Start();
+			Console.Write("Поиск... ");
 			System.Threading.Thread.Sleep(delay);
 			scanner.Stop();
-			return scanner.Sensors;
+			Console.WriteLine("DONE");
+			IReadOnlyList<SensorInfo> sensors = scanner.Sensors;
+			if (sensors.Count == 0)
+				throw new Exception("No sensors found!");
+			Console.WriteLine("Найдены сенсоры:");
+			for (int i = 0; i < sensors.Count; i++)
+			{
+				Console.WriteLine(i + "\t" + sensors[i].Name + "\t" + sensors[i].Address);
+			}
+			Console.Write("Выберите сенсор >> ");
+			int sensorNumber = int.Parse(Console.ReadLine());
+			SensorInfo sensorInfo = sensors[sensorNumber];
+			Console.Write("Получение сенсора... ");
+			CallibriSensor sensor = scanner.CreateSensor(sensorInfo) as CallibriSensor;
+			Console.WriteLine("DONE");
+			Console.Write("Освобождение ресурсов сканера... ");
 			scanner.Dispose();
+			Console.WriteLine("DONE");
+			return sensor;
 		}
 	}
 }
